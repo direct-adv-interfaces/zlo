082309724d8c84ac1c3df33ae710230738b7da61
Vera Nekhaeva <heliarian@ppcdev1.yandex.ru>
2016-01-26 13:24:43 +0300

    fix bower path

--- zlo.js
+++ zlo.js
@@ -102,14 +102,18 @@ Zlo.prototype.loadFromNet = function () {
 
     this.createLoadingConfigs();
 
-    console.log('------LOAD FROM NET------ ');
+    console.log('------LOAD FROM NET------ '); 
+    console.log('npm install');
     exec('npm install', function(err, stdout) {
 	if (err) {
             console.error(err);
             process.exit(0);
         }
         console.log('------NPM INSTALL FINISHED ------');
-        exec('bower install', function(err, stdout) {
+
+	var bowerPath = path.resolve(__dirname, 'node_modules/bower/bin/bower');
+        console.log(bowerPath + ' install');
+	exec(bowerPath + ' install', function(err, stdout) {
 	    if (err) {
 		console.error(err);
 		process.exit(0);
