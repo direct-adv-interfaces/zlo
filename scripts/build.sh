b53fdcdbba1976ec4961181ed46ab3330c271590
Alexey Kalinin <hsalkaline@gmail.com>
2018-10-01 17:17:52 +0300

    README и поправленный скрипт

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$DIR/.."
-rm -rf "$ROOT/debian"
+rm -rf "$ROOT/debian" || true
 cp -R $ROOT/node_modules usr/node_modules
